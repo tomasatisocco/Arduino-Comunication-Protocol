97a45a924a215c6d6d249f19ccda31af7e71df6c
Tomas Tisocco <tomastisocco97@gmail.com>
2021-06-07 23:40:47 -0300

    Codigo hecho en clase

--- Protocolo de Comuniccacion/src/main.cpp
+++ Protocolo de Comuniccacion/src/main.cpp
@@ -10,8 +10,17 @@
 #define SW3 8
 #define SW4 9
 
-uint8_t txbuff, checksum, i, header1 = 0xE0, header2 = 0x0E, estado, rxbuff;
-uint16_t lenght = 0x0003;
+#define ESPERANDOE0 0
+#define ESPERANDO0E 1
+#define ESPERANDOLB 2
+#define ESPERANDOHB 3
+#define ESPERANDO3A 4
+#define ESPERANDOPL 5
+
+
+uint8_t txbuff, checksum, i, header1 = 0xE0, header2 = 0x0E, estado, rxBuff[16], txBuff[16], indexWrite, indexRead;
+uint8_t stateRead;
+uint16_t lenght = 0x0003, lenghtPL;
 
 void setup() {
   pinMode(LED1, OUTPUT);
@@ -24,29 +33,71 @@ void setup() {
   pinMode(SW3, INPUT);
   pinMode(SW4, INPUT);
 
+  stateRead = ESPERANDOE0;
+
   Serial.begin(9600);
 
 }
 
 void loop() {
   if (Serial.available()){
-    while(Serial.available()){
-      i = 1;
-      rxbuff = Serial.read();
+    rxBuff[indexRead] = Serial.read();
+    switch(stateRead){
+      case ESPERANDOE0:
+      if( rxBuff[indexRead] == 0xE0 ){
+        stateRead = ESPERANDO0E;
+      }
+      break;
+      case ESPERANDO0E:
+      if( rxBuff[indexRead] == 0x0E ){
+        stateRead = ESPERANDOLB;
+      } else {
+        stateRead = ESPERANDOE0;
+      }
+      break;
+      case ESPERANDOLB:
+      lenghtPL = rxBuff[indexRead];
+      checksum = 0xE0 + 0x0E + rxBuff[indexRead];
+      stateRead = ESPERANDOHB;
+      break;
+      case ESPERANDOHB:
+      stateRead = ESPERANDO3A;
+      lenghtPL = lenghtPL + 256 * rxBuff[indexRead];
+      checksum = checksum + rxBuff[indexRead];
+      break;
+      case ESPERANDO3A:
+      if (rxBuff[indexRead] == 0x3A){
+        checksum = checksum + 0x3A;
+        stateRead = ESPERANDOPL;
+      }
+      break;
+      case ESPERANDOPL:
+      if (lenghtPL > 1){
+        checksum = checksum + rxBuff[indexRead];
+      }
+      lenghtPL--;
+      if (lenghtPL == 0){
+        stateRead = ESPERANDOE0;
+        if (checksum == rxBuff[indexRead]){
+          switch (rxBuff[0]){
+            case 0xF0:
+            Serial.write(header1);
+            Serial.write(header2);
+            Serial.write(0x03);
+            Serial.write(0x00);
+            Serial.write(0x3A);
+            Serial.write(0xF0);
+            Serial.write(0x0D);
+            Serial.write(0x28);
+            break;
+          }
+          indexRead = 0;
+        }
+      }
+      indexRead++;
+      break;
+      default:
+      stateRead = ESPERANDOE0;
     }
-    estado ^= 0x01;
-    digitalWrite(LED1, estado);
-  }
-  if (i == 1){
-    checksum = header1 + header2 + 0x3A + 0x03 + 0xF0 + 0x0D;
-    Serial.write(header1);
-    Serial.write(header2);
-    Serial.write(0x03);
-    Serial.write(0x00);
-    Serial.write(0x3A);
-    Serial.write(0xF0);
-    Serial.write(0x0D);
-    Serial.write(checksum);
-    i = 0;
   }
 }

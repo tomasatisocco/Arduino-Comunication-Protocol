9e3924649239d68a659a4b7a33810e65937ee9d0
Tomas Tisocco <tomastisocco97@gmail.com>
2021-06-10 21:27:17 -0300

    El codigo cumple con todas sus funciones pero falta mejorar que tan rapido se cambian las variables

--- Protocolo de Comuniccacion/src/main.cpp
+++ Protocolo de Comuniccacion/src/main.cpp
@@ -17,12 +17,24 @@
 #define ESPERANDO3A 4
 #define ESPERANDOPL 5
 
+#define ALIVE 0xF0
+#define DERECHA 0xA0
+#define IZQUIERDA 0xA1
+#define ABAJO 0xA2
+#define ARRIBA 0xA3
+#define LANZAMIENTO 0xD1
+#define NOCONOCIDO 0xXX
 
-uint8_t txbuff, checksum, i, header1 = 0xE0, header2 = 0x0E, estado, rxBuff[16], txBuff[16], indexWrite, indexRead;
-uint8_t stateRead;
+uint8_t txbuff, checksum, i, header1 = 0xE0, header2 = 0x0E, estado, rxBuff[16], txBuff[9], indexWrite, indexRead;
+uint8_t stateRead, respuesta, botones, botonesAnterior;
 uint16_t lenght = 0x0003, lenghtPL;
+unsigned long Tiempo, UTDebounce, UTRebote;
+
+void Respuesta();
+void leerBotones();
 
 void setup() {
+
   pinMode(LED1, OUTPUT);
   pinMode(LED2, OUTPUT);
   pinMode(LED3, OUTPUT);
@@ -34,12 +46,17 @@ void setup() {
   pinMode(SW4, INPUT);
 
   stateRead = ESPERANDOE0;
+  txBuff[0] = 0xE0;
+  txBuff[1] = 0x0E;
+  txBuff[3] = 0x00;
+  txBuff[4] = 0x3A;
 
   Serial.begin(9600);
 
 }
 
 void loop() {
+  leerBotones();
   if (Serial.available()){
     rxBuff[indexRead] = Serial.read();
     switch(stateRead){
@@ -79,25 +96,146 @@ void loop() {
       if (lenghtPL == 0){
         stateRead = ESPERANDOE0;
         if (checksum == rxBuff[indexRead]){
-          switch (rxBuff[0]){
-            case 0xF0:
-            Serial.write(header1);
-            Serial.write(header2);
-            Serial.write(0x03);
-            Serial.write(0x00);
-            Serial.write(0x3A);
-            Serial.write(0xF0);
-            Serial.write(0x0D);
-            Serial.write(0x28);
-            break;
+          Respuesta();
           }
           indexRead = 0;
+        } else {
+          indexRead++;
         }
-      }
-      indexRead++;
       break;
       default:
       stateRead = ESPERANDOE0;
     }
   }
+  Tiempo = millis();
+  if (Tiempo - UTRebote >= 800){
+    digitalWrite(LED1,LOW);
+    digitalWrite(LED2,LOW);
+    digitalWrite(LED3,LOW);
+    digitalWrite(LED4,LOW);
+  }
+}
+
+void Respuesta(){
+  switch (rxBuff[0]){
+    case ALIVE:
+    txBuff[2] = 0x03;
+    txBuff[5] = 0xF0;
+    txBuff[6] = 0x0D;
+    txBuff[7] = 0x28;
+    txBuff[8] = 0x00;
+    respuesta = 0x01;
+    break;
+    case LANZAMIENTO:
+    if (rxBuff[1] == 0x00){
+      digitalWrite(LED_BUILTIN,LOW);
+      estado = 0x00;
+      respuesta = 0x00;
+    }
+    if (rxBuff[1] == 0x01){
+      digitalWrite(LED_BUILTIN,HIGH);
+    }
+    break;
+    case DERECHA:
+    estado = 0x01;
+    respuesta = 0x00;
+    UTRebote = millis();
+    break;
+    case IZQUIERDA:
+    estado = 0x02;
+    respuesta = 0x00;
+    UTRebote = millis();
+    break;
+    case ABAJO:
+    estado = 0x04;
+    respuesta = 0x00;
+    UTRebote = millis();
+    break;
+    case ARRIBA:
+    estado = 0x08;
+    respuesta = 0x00;
+    UTRebote = millis();
+    break;
+    default:
+    respuesta = 0x00;
+  }
+  if (respuesta == 0x00){
+    digitalWrite(LED1,estado & 0x01);
+    digitalWrite(LED2,estado & 0x02);
+    digitalWrite(LED3,estado & 0x04);
+    digitalWrite(LED4,estado & 0x08);
+    } else {
+    indexWrite = 0x00;
+    while( Serial.availableForWrite() ){
+      Serial.write(txBuff[indexWrite]);
+      indexWrite++;
+    }
+  }
+}
+void leerBotones(){
+  if (digitalRead(SW1) || digitalRead(SW2) || digitalRead(SW3) || digitalRead(SW4)){
+    botones = 0x00;
+    if( digitalRead(SW1))
+      botones |= 0x01;
+    if( digitalRead(SW2))
+      botones |= 0x02;
+    if( digitalRead(SW3))
+      botones |= 0x04;
+    if( digitalRead(SW4))
+      botones |= 0x08;
+    Tiempo = millis();
+    if( (Tiempo - UTDebounce) >= 30){
+      if (botones ^ botonesAnterior){
+        botonesAnterior = botones;
+        if (botonesAnterior & 0x01 ){
+          txBuff[2] = 0x02;
+          txBuff[5] = 0xD2;
+          txBuff[6] = 0xFC;
+        }
+        if (botonesAnterior & 0x02 ){
+          txBuff[2] = 0x04;
+          txBuff[5] = 0xD0;
+          txBuff[6] = 0x01;
+          txBuff[7] = 0x00;
+          txBuff[8] = 0xFD;
+        }
+        if (botonesAnterior & 0x04 ){
+          txBuff[2] = 0x04;
+          txBuff[5] = 0xD0;
+          txBuff[6] = 0xFF;
+          txBuff[7] = 0xFF;
+          txBuff[8] = 0xFA;
+        }
+        if (botonesAnterior & 0x08 ){
+          txBuff[2] = 0x02;
+          txBuff[5] = 0xD1;
+          txBuff[6] = 0xFB;
+          txBuff[7] = 0x00;
+          txBuff[8] = 0x00;
+        }
+      /*  if (Serial.availableForWrite()){
+          Serial.write(txBuff[0]);
+          Serial.write(txBuff[1]);
+          Serial.write(txBuff[2]);
+          Serial.write(txBuff[3]);
+          Serial.write(txBuff[4]);
+          Serial.write(txBuff[5]);
+          Serial.write(txBuff[6]);
+          Serial.write(txBuff[7]);
+          Serial.write(txBuff[8]);
+        }*/
+        indexWrite = 0x00;
+        while (Serial.availableForWrite()){
+          Serial.write(txBuff[indexWrite]);
+          indexWrite++;
+        }
+        UTDebounce = 0;
+        } else {
+          UTDebounce = millis();
+        }
+      }
+    } else {
+      botonesAnterior = 0x00;
+      UTDebounce = millis();
+    }
 }

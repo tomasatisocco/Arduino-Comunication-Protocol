7c523608a9bf580b0908979ad97713056b9e01a6
Tomas Tisocco <tomastisocco97@gmail.com>
2021-06-28 02:24:38 -0300

    Intento de guardadp y reproduccion de secuencia de leds

--- Protocolo de Comuniccacion/src/main.cpp
+++ Protocolo de Comuniccacion/src/main.cpp
@@ -24,6 +24,10 @@
 #define ARRIBA 0xA3
 #define LANZAMIENTO 0xD1
 
+#define RESET 0
+#define SAVE 1
+#define STOP 2
+
 void Respuesta();
 void LeerBotones();
 void PutByteIntx(uint8_t byte);
@@ -31,17 +35,16 @@ void PutHeaderIntx();
 void ChangeVariable();
 void Shot();
 void Add(uint16_t cant);
+void SaveSec(uint8_t mode);
+void PlaySec();
 
-<<<<<<< HEAD
-uint8_t txbuff, checksum, i, header1 = 0xE0, header2 = 0x0E, estado, rxBuff[32], txBuff[32], indexWrite, indexRead;
-uint8_t stateRead, respuesta, botones, botonesAnterior;
-uint16_t lenghtPL;
-=======
 uint8_t rxBuff[32], txBuff[32], indexWriteTX, indexReadTX, indexReadRX, indexWriteRX;
-uint8_t stateRead, respuesta, botones, botonesAnterior, checksumRX, checksumTX, estado;
+uint8_t stateRead, botones, botonesAnterior, checksumRX, checksumTX, estado;
 uint16_t lenghtPL, lenghtPLSaved;
->>>>>>> actualizacion
-unsigned long tiempo, uTDebounce, uTRebote, uTBtn, uTDato;
+unsigned long tiempo, uTDebounce, uTRebote, uTBtn, uTDato, uTPlay;
+uint32_t secMemory, readSec;
+unsigned long timeMemory[10];
+uint8_t indexMemoryWrite, indexMemoryRead, play, stop;
 
 void LeerBotones(){
   if (digitalRead(SW1) || digitalRead(SW2) || digitalRead(SW3) || digitalRead(SW4)){
@@ -68,42 +71,20 @@ void LeerBotones(){
           Add(-1);
         }
         if (botonesAnterior & 0x08 ){
-<<<<<<< HEAD
-          txBuff[2] = 0x02;
-          txBuff[5] = 0xD1;
-          txBuff[6] = 0xFB;
-=======
           Shot();
->>>>>>> actualizacion
         }
         uTBtn = millis();
       } else {
           uTDebounce = millis();
-          if (((botonesAnterior & 0x02) || (botonesAnterior & 0x04)) && ((uTDebounce - uTBtn) > 500)){
-<<<<<<< HEAD
-            if ((uTDebounce - uTBtn) > 2000){
-              if (botonesAnterior & 0x02){
-                txBuff[6] = 0x0A;
-                txBuff[7] = 0x00;
-                txBuff[8] = 0x06;
-              }
-              if (botonesAnterior & 0x04){
-                txBuff[6] = 0xF6;
-                txBuff[7] = 0xFF;
-                txBuff[8] = 0xF1;
-              }
-            }
-            indexWrite = 0;
-            while((indexWrite < 9) && Serial.availableForWrite()){
-              Serial.write(txBuff[indexWrite]);
-              indexWrite++;
-=======
+          if ((uTDebounce - uTBtn) > 500){
             if (botonesAnterior & 0x02){
               Add(10);
             }
             if (botonesAnterior & 0x04){
               Add(-10);
->>>>>>> actualizacion
+            }
+            if (botonesAnterior & 0x01){
+              play = 0x01;
             }
           }
         }
@@ -157,6 +138,59 @@ void PutHeaderIntx(){
   checksumTX = 0x0E + 0xE0;
 }
 
+void SaveSec(uint8_t mode){
+  switch (mode) {
+    case RESET:
+      secMemory = 0x00;
+      indexMemoryWrite = 0;
+      readSec = 0x01;
+      stop = 0x01;
+    break;
+    case SAVE:
+      if (secMemory == 0){
+        secMemory |= estado;
+        secMemory <<= 4;
+        readSec <<= 4;
+        timeMemory[indexMemoryWrite] = 0;
+        indexMemoryWrite++;
+      } else {
+        secMemory |= estado;
+        secMemory <<= 4;
+        readSec <<= 4;
+        tiempo = millis();
+        timeMemory[indexMemoryWrite] = (tiempo - uTRebote);
+        indexMemoryWrite++;
+      }
+    break;
+    case STOP:
+      stop = 0x00;
+      indexMemoryRead = 0x00;
+    break;
+  }
+}
+
+void PlaySec(){
+  tiempo = millis();
+  if((tiempo - uTPlay) >= timeMemory[indexMemoryRead]){
+    digitalWrite(LED1, secMemory & readSec);
+    readSec >>= 1;
+    digitalWrite(LED2, secMemory & readSec);
+    readSec >>= 1;
+    digitalWrite(LED3, secMemory & readSec);
+    readSec >>= 1;
+    digitalWrite(LED4, secMemory & readSec);
+    readSec >>= 1;
+    indexMemoryRead++;
+    if(readSec <= 1){
+      readSec = 0x01;
+      readSec <<= (indexMemoryRead * 4);
+      indexMemoryRead = 0;
+      play = 0x00;
+    }
+    uTPlay = millis();
+  }
+}
+
 void Respuesta(){
   switch (rxBuff[(indexReadRX - lenghtPLSaved)]){
     case ALIVE:
@@ -167,47 +201,43 @@ void Respuesta(){
       PutByteIntx(0xF0);
       PutByteIntx(0x0D);
       PutByteIntx(checksumTX);
-      respuesta = 0x01;
     break;
     case LANZAMIENTO:
       if (rxBuff[(indexReadRX - (lenghtPLSaved - 1))] == 0x00){
-        digitalWrite(LED_BUILTIN,LOW);
+  //      digitalWrite(LED_BUILTIN,LOW);
         estado = 0x00;
-        respuesta = 0x00;
+        SaveSec(STOP);
       }
       if (rxBuff[(indexReadRX - (lenghtPLSaved - 1))] == 0x01){
-        digitalWrite(LED_BUILTIN,HIGH);
+//        digitalWrite(LED_BUILTIN,HIGH);
+        SaveSec(RESET);
       }
     break;
     case DERECHA:
       estado = 0x01;
-      respuesta = 0x00;
+      SaveSec(SAVE);
       uTRebote = millis();
     break;
     case IZQUIERDA:
       estado = 0x02;
-      respuesta = 0x00;
+      SaveSec(SAVE);
       uTRebote = millis();
     break;
     case ABAJO:
       estado = 0x04;
-      respuesta = 0x00;
+      SaveSec(SAVE);
       uTRebote = millis();
     break;
     case ARRIBA:
       estado = 0x08;
-      respuesta = 0x00;
+      SaveSec(SAVE);
       uTRebote = millis();
     break;
-    default:
-      respuesta = 0x00;
   }
-  if (!respuesta){
-    digitalWrite(LED1,estado & 0x01);
-    digitalWrite(LED2,estado & 0x02);
-    digitalWrite(LED3,estado & 0x04);
-    digitalWrite(LED4,estado & 0x08);
-    }
+  digitalWrite(LED1,estado & 0x01);
+  digitalWrite(LED2,estado & 0x02);
+  digitalWrite(LED3,estado & 0x04);
+  digitalWrite(LED4,estado & 0x08);
 }
 
 void setup() {
@@ -222,6 +252,8 @@ void setup() {
   pinMode(SW3, INPUT);
   pinMode(SW4, INPUT);
 
+  readSec = 0x01;
+  secMemory = 0x00;
   stateRead = ESPERANDOE0;
   Serial.begin(9600);
 
@@ -229,6 +261,9 @@ void setup() {
 
 void loop() {
   LeerBotones();
+  if (play == 0x01){
+    PlaySec();
+  }
   while (Serial.available()){
     rxBuff[indexWriteRX++] = Serial.read();
     indexWriteRX &= (32 - 1);
@@ -237,44 +272,18 @@ void loop() {
     uTDato = millis();
     switch(stateRead){
       case ESPERANDOE0:
-<<<<<<< HEAD
-        if( rxBuff[indexRead] == 0xE0 ){
-          stateRead = ESPERANDO0E;
-        }
-      break;
-      case ESPERANDO0E:
-        if( rxBuff[indexRead] == 0x0E ){
-=======
         if( rxBuff[indexReadRX++] == 0xE0 ){
           stateRead = ESPERANDO0E;
         }
       break;
         case ESPERANDO0E:
         if( rxBuff[indexReadRX++] == 0x0E ){
->>>>>>> actualizacion
           stateRead = ESPERANDOLB;
         } else {
           stateRead = ESPERANDOE0;
         }
       break;
       case ESPERANDOLB:
-<<<<<<< HEAD
-        lenghtPL = rxBuff[indexRead];
-        checksum = 0xE0 + 0x0E + rxBuff[indexRead];
-        stateRead = ESPERANDOHB;
-      break;
-      case ESPERANDOHB:
-        stateRead = ESPERANDO3A;
-        lenghtPL = lenghtPL + 256 * rxBuff[indexRead];
-        checksum = checksum + rxBuff[indexRead];
-      break;
-      case ESPERANDO3A:
-        if (rxBuff[indexRead] == 0x3A){
-          checksum = checksum + 0x3A;
-          stateRead = ESPERANDOPL;
-        } else {
-          stateRead = ESPERANDOE0;
-=======
         lenghtPL = rxBuff[indexReadRX];
         lenghtPLSaved = (rxBuff[indexReadRX] - 1);
         checksumRX = 0xE0 + 0x0E + rxBuff[indexReadRX];
@@ -291,32 +300,18 @@ void loop() {
         if (rxBuff[indexReadRX++] == 0x3A){
           checksumRX = checksumRX + 0x3A;
           stateRead = ESPERANDOPL;
->>>>>>> actualizacion
         }
       break;
       case ESPERANDOPL:
         if (lenghtPL > 1){
-<<<<<<< HEAD
-          checksum = checksum + rxBuff[indexRead];
-=======
           checksumRX = checksumRX + rxBuff[indexReadRX];
->>>>>>> actualizacion
         }
         lenghtPL--;
         if (lenghtPL == 0){
           stateRead = ESPERANDOE0;
-<<<<<<< HEAD
-          if (checksum == rxBuff[indexRead]){
-            Respuesta();
-            }
-          indexRead = 0;
-          } else {
-            indexRead++;
-=======
           if (checksumRX == rxBuff[indexReadRX]){
             Respuesta();
           }
->>>>>>> actualizacion
         }
         indexReadRX++;
       break;
@@ -326,14 +321,17 @@ void loop() {
     indexReadRX &= (32 - 1);
   }
   tiempo = millis();
-  if (tiempo - uTRebote >= 800){
+  /*if (tiempo - uTRebote >= 800){
     digitalWrite(LED1,LOW);
     digitalWrite(LED2,LOW);
     digitalWrite(LED3,LOW);
     digitalWrite(LED4,LOW);
-  }
+  }*/
   if (tiempo - uTDato > 15){
     stateRead =  ESPERANDOE0;
+  }
+  if ((tiempo - uTRebote > 6000) && (stop = 0x01)){
+    SaveSec(STOP);
   }
   if(indexWriteTX != indexReadTX){
     if(Serial.availableForWrite()){
